505cb4bdede2829c69be88acc873b3647b9a4f82
ThibaultCougrand <thibault.cougrand@gmail.com>
2018-12-11 22:35:33 +0100

    Ajout des commentaires de l'exercices 1 et 2

--- Exercice1/js/main.js
+++ Exercice1/js/main.js
@@ -13,19 +13,19 @@
 
 ○ améliorer l'UI/UX pour que cela ressemble à une vraie calculatrice */
 
-refresh();
+refresh();//réinitialise la saisi au rafraichissement de la page et lancement du script
 
-function insertNumber(elemID, text) {
-    var elem = document.getElementById(elemID);
-    elem.value += text;
+function insertNumber(elemID, text) {//fonction pour envoyer la valeur d'un bouton dans la valeur de la zone de calcul.
+    var elem = document.getElementById(elemID);//recupère la zone de calcul dans une variable.
+    elem.value += text;//et on lui assigne la valeur souhaité.
+}//la fonction est appeller dans le onClick des boutons en html.
+
+function calculateThis() {// fonction pour calculer les éléments de la zone de clalcul.
+    var input = document.getElementById('js-valeur').value;//on enferme la valeur contenu dans la zone de calcul dans une variable.
+    var results = eval(input);//on créer une variable avec le calcul de notre précédente valeur.
+    document.getElementById('js-valeur').value = results;// et on écrase la valeur de la zone de calcul par le resultat.
 }
 
-function calculateThis() {
-    var input = document.getElementById('js-valeur').value;
-    var results = eval(input);
-    document.getElementById('js-valeur').value = results;
-}
-
-function refresh() {
-    document.getElementById('js-valeur').value = "";
+function refresh() {//fonction pour rafraichir la zone de calcul.
+    document.getElementById('js-valeur').value = "";//écrase sa valeur par une string vide.
 }
--- Exercice2/js/main.js
+++ Exercice2/js/main.js
@@ -15,41 +15,41 @@ Nombre mystère
 
 ○ pouvoir choisir le nombre de tentatives lecture */
 
-var continu = true;
-var error = true;
-var error2 = true;
-while (continu) {
-    while (error) {
-        var min = Number(prompt("Choisir nombre minimum"));
-        var max = Number(prompt("Choisir nombre maximum"));
-        if (min > max || min < 0 || isNaN(min) || isNaN(max)) {
-            alert("Vos saisi sont incorectes");
-        } else {
-            while(error2){
-                var tentatives = Number(prompt("Choisir un nombre de tentatives"));
-                if (tentatives >= 0){
-                     var randomNumber = Math.floor(Math.random() * (max - min) + min);
-                    error2 = false;    
-                }else {
-                    alert('Le nombre de tentatives doit être positif !')
+var continu = true;//Boolean pour relancer le jeu en cas d'echec.
+var error = true;//Boolean pour relancer la saisi des nombres min et max en cas de mauvaise saisi.
+var error2 = true;//Boolean pour relancer la saisi du nombre de tentatives en cas de mauvaise saisi.
+while (continu) {//Boucle pour relancer le jeu en cas d'échec.
+    while (error) {//Boucle pour relancer la saisi des nombre min et max.
+        var min = Number(prompt("Choisir nombre minimum"));//saisi du nombre minimum.
+        var max = Number(prompt("Choisir nombre maximum"));//saisi du nombre maximum.
+        if (min >= max || min < 0 || isNaN(min) || isNaN(max)) {//si le nombre min est supérieur ou égale au max ou le nombre min est inférieur à 0 ou les saisi ne sont pas des nombres
+            alert("Vos saisi sont incorectes");//message d'erreur.
+        } else {//sinon
+            while(error2){//on rentre dans la boucle pour la saisi du nombre de tentatives.
+                var tentatives = Number(prompt("Choisir un nombre de tentatives"));//on la saisi.
+                if (tentatives <= 0 || isNaN(tentatives)){//si tentative inférieur à 0 ou pas un nombre
+                    alert('Le nombre de tentatives doit être positif !'); //message d'erreur.
+                }else {//sinon
+                    var randomNumber = Math.floor(Math.random() * (max - min) + min);//on genère le nombre aléatoire entre les saisis choisi.
+                    error2 = false;//et on sort de la boucle.
                 }    
             }
-            error = false;
+            error = false;//on sort également de la précédente boucle.
         }
     }
-    for (i = 0; i < tentatives;i++ ) {
-        var input = Number(prompt(`Quel est le nombre mystère ? Vous avez ${tentatives - i} tentatives restantes`));
-        if (input > randomNumber && input <= max) {
-            alert("Il est plus petit");
-        } else if (input < randomNumber && input >= min) {
-            alert("Il est plus grand");
-        } else if (input > max || input < min) {
-            alert("Votre saisi n'est pas contenu entre votre min et max défini plus tôt.");
-            i--;
-        } else {
-            alert("Bravo Vous avez trouvé");
-            i = tentatives;
+    for (i = 0; i < tentatives;i++ ) {//une boucle qui se lance le nombre de tentatives demandées.
+        var input = Number(prompt(`Quel est le nombre mystère ? Vous avez ${tentatives - i} tentatives restantes`));//saisi du nombre par le joueur (affiche le nombre de tentatives restantes)
+        if (input > randomNumber && input <= max) {//si la saisi est supérieur au nombre généré et inférieur ou égale au max alors
+            alert("Il est plus petit");//il affiche plus petit.
+        } else if (input < randomNumber && input >= min) {//si il est plus petit et plus grand que le min alors
+            alert("Il est plus grand");//il affiche plus grand.
+        } else if (input > max || input < min) {//si il est plus grand que le max ou plus petit que le min
+            alert("Votre saisi n'est pas contenu entre votre min et max défini plus tôt.");//message d'erreur
+            i--;//et on recommence l'essai.
+        } else {//sinon
+            alert("Bravo Vous avez trouvé");//victoire.
+            i = tentatives;//on sort de la boucle for.
         }
     }
-    continu = false;
+    continu = false;//et de la boucle du jeu.
 }
